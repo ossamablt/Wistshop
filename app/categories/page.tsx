689b249060706692d73018a0d7b52656a3064305
ossamablt <majidblt0777@gmail.com>
2025-07-08 16:31:51 +0100

    ddd

--- app/categories/page.tsx
+++ app/categories/page.tsx
@@ -1,3 +1,9 @@
+// ✅ Correct
+import { initializeApp } from 'firebase/app';
+import { getDatabase, ref, onValue } from 'firebase/database';
+// ✅ Correct
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/database';
 import { CategoriesPage } from "@/components/pages/categories-page"
 
 export default function Categories() {
